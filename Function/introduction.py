cf3c2290fd0f07dcd13b632be264bbc2905078cc
dpdahal <dp48dahal@gmail.com>
2022-06-01 10:36:12 +0545

    function introduction

--- Function/introduction.py
+++ Function/introduction.py
@@ -1,163 +1,145 @@
-# function two types
-# inbuilt function
 # user defined function
 
-# inbuilt function
-# abs()
-# print()
-# input()
-# dir()
-# help()
-
-# define function
-# def users():
-#     # function body part
-#     print("I am users function")
-#
-#
-# # call function
-# users()
-
-
-# def add(x, y):
-#     return x + y
-#
-#
-# print(add(20, 30))
-# print(add(56, 30))
-# add
-# sub
-# mul
-# div
-# modules
-# WAP to check enter number ever or odd
-# def test(a):
-#     print(a)
-# c = int(input("enter the number"))
-# test(c)
-
-#
-# def users(name, age):
-#     print("Name:", name)
-#     print("Age:", age)
-#
-#
-# users("John", 20)
-# users("Sophia", 50)
-# * args
-# ** kwargs
-
-# def students(*names, **data):
-#     print(names)
-#     print(data)
-#
-#
-# students('ram', 'shyam', 'hari', name='ram', age=20)
+# normal function
 
 # def test():
-#     return "hello"
+#     print("hello world")
+#
+#
+# test()
+
+# function accept value
+
+
+# def user(name, age):
+#     print("hello", name, age)
+#
+#
+# user('ram', 200)
+
+
+# optional value in function
+
+# def users(name, age=0):
+#     print("hello", name)
+#     print("age", age)
+#
+#
+# users('ram', 20)
+
+# def uses(*name, **data):
+#     print(name)
+#     print(data)
+#
+# uses()
+
+
+# function return value
+
+# def test():
+#     a = 10
+#     if a % 2 == 0:
+#         return "even"
+#     else:
+#         return "odd"
 #
 #
 # print(test())
 
-# def get():
-#     return "I am get"
-#
-#
-# def post():
-#     print(get())
-#
-#
-# post()
-#
-# def take_value():
-#     p = int(input("enter p: "))
-#     t = int(input("enter t: "))
-#     r = int(input("enter r: "))
-#     return [p, t, r]
-#
-#
-# def calculate():
-#     data = take_value()
-#     p = data[0]
-#     t = data[1]
-#     r = data[2]
-#     return p * t * r / 100
-#
-#
-# def display():
-#     return calculate()
-#
-#
-# print(display())
+
+# def data():
+#     a = 10
+#     b = 20
+#     return [a, b]
 
 
-# def my_rep(data,times):
-#     return data * times
-#
-# print(my_rep('ram',3))
-
-# def my_rep(data, times):
-#     increment = 0
-#     while increment < times:
-#         print(data)
-#         increment += 1
+# local
+# global
+# x = 30
 #
 #
-# my_rep('ram', 3)
-#
-# print("1.Dell(Rs:20000) 2.Toshiba(Rs:30000) 3.Mac(Rs:40000)")
-#
-#
-# def dell():
-#        # qunatity
-#     q = int(input("enter the quantity: "))
-#
-# def toshiba():
-#     pass
+# def test():
+#     x = 10
 #
 #
-# def mac():
-#     pass
-
-
-#
-# def kalanki():
-#     pass
-#
-# def chabile():
-#     pass
-
+# test()
 
 # def test():
-#     return "test"
+#     print('inside test')
 #
 # print(test)
 
-# 5 = 120
 
-# def fac(n):
-#     if n == 1:
-#         return 1
-#     else:
-#         return n * fac(n - 1)
+# a = lambda x, y: x + y
 #
-# print(fac(5))
+# print(a(10, 30))
 
-# 5-1 =4*5 =20
-# 4-1 =3*20 =60
-# 3-1 =2*60 =120
+# def out():
+#     def inside():
+#         return "inside"
+#
+#     return inside
+#
+# a = out()
+# print(a())
+# print(out()())
+# print(out())
+
+
+# def users():
+#     def name(new_name):
+#         return new_name
+#
+#     return name
+#
+#
+# a = users()
+# print(a('ram'))
+
+
+# def test():
+#     """
+#     this is a test function
+#   """
+#     return "Welcome test function"
+#
+# # print(test())
+# print(test.__doc__)
+# print(test.__name__)
+
+# def get_doc(any_function_here):
+#     def inner_function():
+#         return any_function_here.__doc__
+#
+#     return inner_function
+#
+#
+# @get_doc
+# def users():
+#     """i am users function"""
+#     return "Welcome"
 #
 
-x = 10
+# print(users())
 
 
-def test():
-    global x
-    x = x + 30
-    print(x)
+def zero_check(any_function):
+    def inner_function(x, y):
+        if y == 0:
+            return "Y is zero"
+
+        else:
+            return any_function(x, y)
+
+    return inner_function
 
 
-test()
+@zero_check
+def add(x, y):
+    return x + y
 
-print(x)
+
+print(add(10, 0))
+
+# module
+
